1495393462859206da0a0c0aff1f0adcbc7e12b5
george woods <georgewoods01@gmail.com>
2022-07-04 11:45:38 +0200

    more setup and css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+// faf76924
+
+const API_URL = 'http://www.omdbapi.com?apikey=faf76924';
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch (`${API_URL}&s=${title}`)
+    const data = await response.json();
+
+    console.log(data.Search);
+
+  }
+
+  useEffect(() => {
+    searchMovies('Debbie does Dallas');
+
+
+
+  }, []);
+
   return (
     <h1>App</h1>
   );
